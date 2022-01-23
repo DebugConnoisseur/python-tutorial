258fa2971a9987a4d6091b86b507236e4f1d2c20
prayinglobster <88923986+prayinglobster@users.noreply.github.com>
2022-01-23 21:05:35 +0530

    Create commentsnprint.py

---
+++ tutorial/basics/01.Comments and printing/commentsnprint.py
@@ -0,0 +1,41 @@
+#----------------------------------------------------Comments and Printing--------------------------------------------------------------
+#This is the beginning of the python tutorial
+
+#---------------------------------------------------Comments----------------------------------------------------------------------------
+'''In python, we write comments using hash or "#"
+# is used for single line comments.
+
+Comments are ignored by python and are used for documentation purposes.
+
+Triple quotation marks such as the one used for this description is used for multiline comments.'''
+
+#---------------------------------------------------Printing------------------------------------------------------------------------------
+
+'''
+Python probably has the easiest print statement. It's just a single line and is pretty straightforward.
+
+Let us now try to print the number 2.
+'''
+print(2)
+
+'''
+See? It's just one line.
+
+Ok, now we've printed a number but what about the famous Hello World that almost every tutorial has? You might have that question and we're going to come to it now.
+'''
+print("Hello World")
+
+''' 
+See? Simple, it's just one line too but you must've noticed that I've used quotation marks in this print statement.
+Ask why? It's because the number 2 is an integer whereas Hello World is not a variable and is not defined (we'll come to the 'defining variable part soon).
+So if you write print(Hello World) without the quotes, you would get an error. 
+So to counter this, we use the quotes. 
+
+Python uses the syntax --> print("enter the stuff you want to print") as the print statement
+
+In print statement, you can either use single quotation marks or double quotation marks  
+
+For integers, it's print(enter the number) , without any quotation marks. 
+
+'''
+
