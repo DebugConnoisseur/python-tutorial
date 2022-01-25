a7a6d3590177b382d6911817fcfb395f7a24f41e
prayinglobster <88923986+prayinglobster@users.noreply.github.com>
2022-01-25 21:40:19 +0530

    Create arithmetic_operations.py

---
+++ tutorial/basics/02.Operations in python/arithmetic_operations.py
@@ -0,0 +1,49 @@
+#---------------------------------------------------------------- Arithmetic Operations------------------------------------------------
+
+''' 
+No, no. We are not talking about those operations at hospitals. 
+
+We're talking about arithmetic operations in python, operations like addition, subtraction.
+
+So how do you use those operations in python? That's what we're going to answer today
+
+So first, make yourself familiar with these guys : +,-,*,/,//,%, **.
+
+Wait, I thought you knew about them. Let me tell you what they are.
+
++ ---> Addition (or concatenation as we call it in python)
+- ---> Subtraction
+* ---> Multiplication (or Replication)
+/ ---> Division
+// --> Floor division (no, we are not dividing the floor, it just removes the decimal and gives us the nearest non decimal number)
+% ---> Modulus 
+** --> Exponentiation (just your powers)
+
+Let us now apply these in our programs
+
+'''
+#Addition
+print(3+5)
+
+#Subtraction 
+print(3-5)
+
+#Multiplication
+print(3*5)
+
+#Division
+print(4/8)
+
+#Floor Division
+print(3//5)
+
+#Modulus
+print(3%5)
+
+#Exponentiation
+print(3**5)
+
+'''
+That's it, simple as that.
+
+That's it for arithmetic operators, it's your turn to try them out now.
